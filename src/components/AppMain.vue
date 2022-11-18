d992d2b0963ea58470051613bbd6852f15148b94
Thunderbird93 <antircristian@gmail.com>
2022-11-18 16:01:20 +0100

    [fix] template

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,53 @@
-<template>
-  
-</template>
-
 <script>
-export default {
-    name: 'AppMain',
-    components: {
-        AppCard,
-    },
-    data(){
-        return{
-            store,
+import axios from "axios";
+import AppCard from "./AppCard.vue";
+import {store} from "../data/store";
+
+    export default {
+        name: 'AppMain',
+        components: {
+            AppCard,
+        },
+        data(){
+            return{
+                store,
+            }
+        },
+        methods: {
+            getData(){
+                .get(store.apiUrl)
+                .then((result) => {
+                    store.inData = result.data;
+                })
+                .catch((error)=>{
+                    console.log("error");
+                });
+            }
         }
     }
-}
 </script>
 
-<style>
 
+
+<template>
+    <div class="container">
+        <div
+        class="col-4"
+        v-for="(character, index) in store.inData"
+        :key="index"
+        >
+        <AppCard :chInfo="character" />
+    </div>
+</div>
+</template>
+
+
+<style lang="scss">
+@use "../styles/partials/-variables.scss"
+    body{
+        background-color: $primary-color;
+        .container{
+            height: 100vh;
+        }
+    }
 </style>
