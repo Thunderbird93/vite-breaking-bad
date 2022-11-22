69d52f4849ca2db7a4335f4b5b64e483993bba60
Thunderbird93 <antircristian@gmail.com>
2022-11-22 15:24:31 +0100

    [add] new header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,28 @@ export default {
 </script>
 
 <template>
-  
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="/breakingbad.jpg" alt="logo">
+            </div>
+            <h1>Breaking Bad Api</h1>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
-
+    
+.container{
+    display: flex;
+    align-items: center;
+    margin: 20px;    
+    .logo{
+        height: 50px;
+        margin: 10px;
+        img{
+        height: 100%;
+        }
+    }
+}   
 </style>
