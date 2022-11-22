529d730db74f266493932acb020576aa965d2750
Thunderbird93 <antircristian@gmail.com>
2022-11-22 16:31:38 +0100

    [add] characters

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,11 +1,21 @@
 <script>
+import { store } from '../data/store';
 export default {
     name: 'CharachterList',
+    data(){
+        return{
+            store
+        }
+    }
 }
 </script>
 
 <template>
-  
+  <ul>
+    <li v-for="character in store.charactersList" :key="character.char_id">
+    {{character.name}}
+    </li>
+  </ul>
 </template>
 
 
